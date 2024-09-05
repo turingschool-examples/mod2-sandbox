b8e743b4193b750574516b10164362a7e4c74131
Heather Faerber <heather@turing.com>
2024-09-05 09:43:01 -0600

    Add file for approaching problems many ways practice

---
+++ lessons/approaching_problems_many_ways.js
@@ -0,0 +1,59 @@
+/*
+JS Approaching Problems Many Ways Scratchpad
+
+To run the code in this file, run the command:
+  node lessons/approaching_problems_many_ways.js
+*/ 
+console.log("approaching_problems_many_ways.js is running")
+
+
+/*  PROMPT:
+Write a function findCheapToys that returns an array of toy names that cost less than $20.
+
+Expected output => [ 'Tamagotchi', 'Super Soaker', 'Pogs' ]
+*/
+
+const ninetiesToys = [
+  {
+      name: "Tamagotchi",
+      releaseYear: 1996,
+      price: 15.99
+  },
+  {
+      name: "Furby",
+      releaseYear: 1998,
+      price: 29.99
+  },
+  {
+      name: "Super Soaker",
+      releaseYear: 1989,
+      price: 19.99
+  },
+  {
+      name: "Pogs",
+      releaseYear: 1991,
+      price: 5.99
+  },
+  {
+      name: "Game Boy",
+      releaseYear: 1989,
+      price: 89.99
+  }
+];
+
+
+/* Round 1: forEach
+Solve the prompt using a forEach. You may not use any other iterator methods.
+*/
+
+
+
+/* Round 2: filter and map
+Solve the prompt using a filter and map. You may not use any other iterator methods.
+*/
+
+
+
+/* Round 3: reduce
+Solve the prompt using a reduce. You may not use any other iterator methods.
+*/
